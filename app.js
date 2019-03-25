8c723419fda0ee5fd1a9ce5fcf2265059788ad3a
Jonathan Ming <jming@pkcsecurity.com>
2019-03-25 15:25:26 -0700

    Prettier & package.json updates

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const json2html = require('node-json2html');
-const https = require('https');
-const express = require('express')
-const app = express()
+const json2html = require("node-json2html");
+const https = require("https");
+const express = require("express");
+const app = express();
 
 let port = process.env.PORT;
 if (port == null || port == "") {
@@ -9,35 +9,47 @@ if (port == null || port == "") {
 }
 app.listen(port);
 
-function onReport(req, res) {
-  https.get('https://give.imb.org/api/projects', (resp) => {
-    let data = '';
+function onReport(_, res) {
+  https
+    .get("https://give.imb.org/api/projects", resp => {
+      let data = "";
 
-    resp.on('data', (chunk) => {
-      data += chunk;
+      resp.on("data", chunk => {
+        data += chunk;
+      });
+
+      resp.on("end", () => {
+        const d = JSON.parse(data);
+        const filtered = d.filter(x => x.lmcid == "F9LMC120180200");
+        d.map(x => {
+          x.totalgifts = x.totalgifts.toFixed(2);
+        });
+        d.map(x => {
+          x.refreshdate = new Date().toISOString();
+        });
+
+        const transform = {
+          "<>": "div",
+          html: [
+            { "<>": "h1", text: "${title}" },
+            { "<>": "h2", text: "${lmcid}" },
+            {
+              "<>": "p",
+              text:
+                "$${totalgifts} raised Online by ${unique-generosity-donors-count} Givers"
+            },
+            { "<>": "i", text: "Refreshed on  ${refreshdate}" }
+          ]
+        };
+
+        const html = json2html.transform(filtered, transform);
+
+        res.send(html);
+      });
+    })
+    .on("error", err => {
+      console.log("Error: " + err.message);
     });
-
-    resp.on('end', () => {
-      const d = JSON.parse(data);
-      const filtered = d.filter(x => x.lmcid == "F9LMC120180200");
-      d.map(x => { x.totalgifts = x.totalgifts.toFixed(2); });
-      d.map(x => { x.refreshdate = new Date().toISOString(); });
-
-      const transform = {'<>':'div','html':[
-        {'<>':'h1','text':'${title}'},
-        {'<>':'h2','text':'${lmcid}'},
-        {'<>':'p','text':'$${totalgifts} raised Online by ${unique-generosity-donors-count} Givers'},
-        {'<>':'i','text':'Refreshed on  ${refreshdate}'}
-      ]};
-
-      const html = json2html.transform(filtered,transform);
-
-      res.send(html)
-    });
-
-  }).on("error", (err) => {
-    console.log("Error: " + err.message);
-  });
 }
 
-app.get('/F9LMC120180200', onReport);
+app.get("/F9LMC120180200", onReport);
