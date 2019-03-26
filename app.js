f7528da74bb33fff038e1ea5c1ffe12634d72157
Jonathan Ming <jming@pkcsecurity.com>
2019-03-25 18:16:25 -0700

    Added a switch that toggles auto-reload mode

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const usdFormatter = new Intl.NumberFormat("en-US", {
   currency: "USD"
 });
 
-app.get("/:lmcid", async (req, res) => {
+async function loadPage(req, res, autoreload) {
   try {
     const apiResp = await request({
       method: "GET",
@@ -17,19 +17,27 @@ app.get("/:lmcid", async (req, res) => {
       strictSSL: true,
       json: true
     });
-    const proj = apiResp.find(x => x.lmcid === req.params.lmcid);
+    const lmcid = req.params.lmcid;
+    const proj =
+      lmcid === "F9LMCO"
+        ? apiResp.find(x => x.id === 122)
+        : apiResp.find(x => x.lmcid === lmcid);
     res.render("index", {
       imageurl: proj.url,
       title: proj.title,
       goal: usdFormatter.format(proj.totalgoal),
       raised: usdFormatter.format(proj.totalgifts),
-      percent: Math.floor((proj.totalgifts / proj.totalgoal) * 100),
-      donorcount: proj["unique-generosity-donors-count"]
+      percent: ((proj.totalgifts / proj.totalgoal) * 100).toFixed(2),
+      donorcount: proj["unique-generosity-donors-count"],
+      autoreload: autoreload
     });
   } catch (e) {
     console.error(`Error requesting Generosity API: ${e.message}`);
     res.status(500).send("Error retrieving project data!");
   }
-});
+}
+
+app.get("/:lmcid", async (req, res) => loadPage(req, res, false));
+app.get("/r/:lmcid", async (req, res) => loadPage(req, res, true));
 
 app.listen(process.env.PORT || 8080);
